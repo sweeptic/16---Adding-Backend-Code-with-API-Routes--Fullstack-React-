3b884768f584baba007738b3ecfc34da9b8a7ea9
Attila Horvath <sweeptic@gmail.com>
2025-03-17 14:52:18 +0100

    004 Preparing the Frontend Form

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,34 @@
+import { useRef } from 'react';
+
 function HomePage() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="mail">Your email address</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback" ref={feedbackInputRef}>
+            Your feedback
+          </label>
+          <textarea id="email" rows="5"></textarea>
+        </div>
+
+        <button>Send Feedback</button>
+      </form>
     </div>
   );
 }
